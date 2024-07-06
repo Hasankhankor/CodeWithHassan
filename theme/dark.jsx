202e181ad53fff70e9a0135a294be13a79d0de12
Hasan Tariq <hkhankor@gmail.com>
2024-07-06 23:40:02 +0500

    updated

---
+++ theme/dark.jsx
@@ -0,0 +1,10 @@
+export const dark = {
+    background: "linear-gradient(10deg, #0a0a0a, #000000)",
+    foreground: "hsl(210 20% 98%)",
+    caret: "hsl(210 20% 98%)",
+    selection: "linear-gradient(180deg, #1a1a1a, #0a0a0a)",
+    selectionMatch: "linear-gradient(180deg, #1a1a1a, #0a0a0a)",
+    lineHighlight: "transparent",
+    gutterBackground: "linear-gradient(180deg, #0b0b0b, #010101)",
+    gutterForeground: "hsl(210 20% 98%)",
+};
